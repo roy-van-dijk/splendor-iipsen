f917e17aefe6754609a5061b2f73f539bf99a3b4
Alexander van Dam <alexiaan.vd@hotmail.com>
2018-05-29 12:46:32 +0200

    vergeten Merge conflict gefixt

--- src/application/domain/Noble.java
+++ src/application/domain/Noble.java
@@ -1,40 +1,4 @@
-package application.domain;
 
-import java.util.Map;
-/**
- * 
- * @author 	Sanchez
- * edit		Tom
- *
- */
-public class Noble {
-	private int prestigeValue;
-	private String illustration; 
-	private Map<Gem, Integer> requirements;
-	
-	public Noble(int prestigeValue, String illustration, Map<Gem, Integer> requirements) {
-		this.prestigeValue = prestigeValue;
-		this.illustration = illustration;
-		this.requirements = requirements;
-	}
-
-	public int getPrestigeValue() {
-		return prestigeValue;
-	}
-
-	//requirements are different from the cards requirements. 
-	//nobles are bought automatically at the end of a turn with only bonesus, not tokens.
-	public Map<Gem, Integer> getRequirements() {
-		return requirements;
-	}
-
-	public String getIllustration() {
-		return illustration;
-	}
-	
-	
-}
-=======
 package application.domain;
 
 import java.rmi.Remote;
@@ -53,3 +17,4 @@ public interface Noble extends Remote {
 	public int getIllustrationID() throws RemoteException;
 	
 }
+	
