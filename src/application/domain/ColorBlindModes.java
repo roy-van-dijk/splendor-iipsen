4bb25850e359dcd2aecf79d43767b21276afeab8
Sanchez <sanchezhuisraad@gmail.com>
2018-05-29 13:38:45 +0200

    Added colorblind support for gems

---
+++ src/application/domain/ColorBlindModes.java
@@ -0,0 +1,8 @@
+package application.domain;
+
+public enum ColorBlindModes {
+	NORMAL,
+	DEUTERANOPIA,
+	PROTANOPIA,
+	TRITANOPIA;
+}
--- src/application/domain/Gem.java
+++ src/application/domain/Gem.java
@@ -1,5 +1,9 @@
 package application.domain;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * @author Sanchez
  *
@@ -10,5 +14,63 @@ public enum Gem {
 	RUBY,
 	ONYX,
 	SAPPHIRE,
-	JOKER
+	JOKER;
+	
+	
+	/**
+	 * @author Sanchez
+	 * @param gem
+	 * @param mode
+	 * @return
+	 */
+	public int hueOffset(Gem gem, ColorBlindModes mode)
+	{
+		Map<ColorBlindModes, Integer> hueMap = new HashMap<>();
+		switch(this)
+		{
+			case DIAMOND:
+				hueMap.put(ColorBlindModes.NORMAL, 0);
+				hueMap.put(ColorBlindModes.DEUTERANOPIA, 0);
+				hueMap.put(ColorBlindModes.PROTANOPIA, 0);
+				hueMap.put(ColorBlindModes.TRITANOPIA, 0);
+			break;
+				
+			case EMERALD:
+				hueMap.put(ColorBlindModes.NORMAL, 0);
+				hueMap.put(ColorBlindModes.DEUTERANOPIA, 20);
+				hueMap.put(ColorBlindModes.PROTANOPIA, 30);
+				hueMap.put(ColorBlindModes.TRITANOPIA, 40);
+			break;
+				
+			case RUBY:
+				hueMap.put(ColorBlindModes.NORMAL, 0);
+				hueMap.put(ColorBlindModes.DEUTERANOPIA, 20);
+				hueMap.put(ColorBlindModes.PROTANOPIA, 30);
+				hueMap.put(ColorBlindModes.TRITANOPIA, 40);
+			break;
+				
+			case ONYX:
+				hueMap.put(ColorBlindModes.NORMAL, 0);
+				hueMap.put(ColorBlindModes.DEUTERANOPIA, 20);
+				hueMap.put(ColorBlindModes.PROTANOPIA, 30);
+				hueMap.put(ColorBlindModes.TRITANOPIA, 40);
+			break;
+				
+			case SAPPHIRE:
+				hueMap.put(ColorBlindModes.NORMAL, 0);
+				hueMap.put(ColorBlindModes.DEUTERANOPIA, 0);
+				hueMap.put(ColorBlindModes.PROTANOPIA, 0);
+				hueMap.put(ColorBlindModes.TRITANOPIA, 0);
+			break;
+				
+			case JOKER:
+				hueMap.put(ColorBlindModes.NORMAL, 0);
+				hueMap.put(ColorBlindModes.DEUTERANOPIA, 20);
+				hueMap.put(ColorBlindModes.PROTANOPIA, 30);
+				hueMap.put(ColorBlindModes.TRITANOPIA, 40);
+			break;
+		}
+		
+		return hueMap.get(mode);
+	}
 }
