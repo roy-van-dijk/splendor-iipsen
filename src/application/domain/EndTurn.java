3b2eeadb22c5a619d7efd422d735edc716e140e5
Nickmanbear <misterkees@hotmail.com>
2018-06-08 17:16:17 +0200

    Kaart kopen ost nu daadwerkelijk tokens

--- src/application/domain/EndTurn.java
+++ src/application/domain/EndTurn.java
@@ -3,6 +3,7 @@ package application.domain;
 import java.rmi.RemoteException;
 import java.util.List;
 import java.util.Map;
+import java.util.Map.Entry;
 
 
 public interface EndTurn {
@@ -18,5 +19,7 @@ public interface EndTurn {
 	void endTurn() throws RemoteException;
 
 	void cleanUpTurn() throws RemoteException;
+
+	int[] neededTokens(Entry<Gem, Integer> cost, Map<Gem, Integer> playerGems, Player player) throws RemoteException;
 
 }
--- src/application/domain/EndTurnImpl.java
+++ src/application/domain/EndTurnImpl.java
@@ -2,6 +2,7 @@ package application.domain;
 
 import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.io.Serializable;
 import java.rmi.RemoteException;
 import java.util.ArrayList;
 import java.util.List;
@@ -10,7 +11,7 @@ import java.util.Map;
 import application.controllers.ReturnTokenController;
 import application.services.SaveGameDAO;
 
-public class EndTurnImpl implements EndTurn {
+public class EndTurnImpl implements EndTurn, Serializable {
 	
 	private Game game;
 	
@@ -52,8 +53,6 @@ public class EndTurnImpl implements EndTurn {
 
 	@Override
 	public boolean compareNobleAndBonusGems(Map<Gem, Integer> nobleCost, Map<Gem, Integer> totalBonusGems) {
-		System.out.println(nobleCost);
-		System.out.println(totalBonusGems);
 		for(Map.Entry<Gem, Integer> nobleCostEntry : nobleCost.entrySet()) {
 		//for(Gem gemType : Gem.values()) 
 			if(nobleCostEntry.getValue() > totalBonusGems.get(nobleCostEntry.getKey())) {
@@ -90,7 +89,7 @@ public class EndTurnImpl implements EndTurn {
 	public void endTurn() throws RemoteException {
 
 		PlayingField playingfield = game.getPlayingField();
-		TempHand temphand = game.getPlayingField().getTempHand();
+		TempHand tempHand = game.getPlayingField().getTempHand();
 		Player player = game.getCurrentPlayer();
 		List<Noble> allNobles= game.getPlayingField().getNobles();
 		Map<Gem, Integer> totalBonusGems = game.getCurrentPlayer().getBonus();
@@ -101,7 +100,7 @@ public class EndTurnImpl implements EndTurn {
 		ReturnTokens model = new ReturnTokens(game.getPlayingField(), game.getCurrentPlayer());
 		ReturnTokenController controller = new ReturnTokenController(model);
 		
-		if(temphand.getSelectedTokensCount() != 0) {
+		if(tempHand.getSelectedTokensCount() != 0) {
 			this.getTokens();
 		}
 		
@@ -117,29 +116,52 @@ public class EndTurnImpl implements EndTurn {
 		/**
 		 * Adds the reservecard to the player
 		 */
-		if(temphand.getReservedCard() != null) {
+		if(tempHand.getReservedCard() != null) {
 			CardRow row = game.getPlayingField().getCardRows().get(1); // Second row
 			Card card = row.getCardSlots()[1];
-			playingfield.removeCard(temphand.getReservedCard());
-			player.addReserverveCard(temphand.getReservedCard());
+			playingfield.removeCard(tempHand.getReservedCard());
+			player.addReserverveCard(tempHand.getReservedCard());
 			game.getCurrentPlayer().reserveCardFromField(row, card);
 			
 		}
 		/**
 		 * Add temphand cards to the player
 		 */
-		if(temphand.getBoughtCard() != null) {
+		if(tempHand.getBoughtCard() != null) {
 			game.cleanUpSelections();
 			//player.returnTokensToField(removedTokens, playingfield);
 			
-			player.addCard(temphand.getBoughtCard());
+			player.addCard(tempHand.getBoughtCard());
+
+			Map<Gem, Integer> playerGems = player.getTokensGemCount();
+			Map<Gem, Integer> costs = tempHand.getBoughtCard().getCosts();
+			
+			for(Map.Entry<Gem, Integer> cost : costs.entrySet()) {
+				
+				int[] totalTokensNeeded = this.neededTokens(cost, playerGems, player;
+				int tokensNeeded = totalTokensNeeded[0];
+				int jokersNeeded =  totalTokensNeeded[1];
+				
+				for(int i = 0; i < tokensNeeded; i++) {
+					for(Token token : player.getTokens()) {
+						if(token.getGemType() == cost.getKey()) {
+							player.getTokens().remove(token);
+							break;
+						}
+					}
+				}		
+				for(int i = 0; i < jokersNeeded; i++) {
+					for(Token token : player.getTokens()) {
+						if(token.getGemType() == Gem.JOKER) {
+							player.getTokens().remove(token);
+							break;
+						}
+					}
+				}	
+			}	
 		}
-
-
-		
 		
 		//TODO take three token
-
 		
 		//game.getPlayers().get(game.getCurrentPlayerIdx()).getOwnedCards().add(game.getTurn().getBoughtCard());
 		//TODO: subtract tokens from player.
@@ -154,6 +176,28 @@ public class EndTurnImpl implements EndTurn {
 		//TODO: Determine next player
 		
 		game.nextTurn();
+	}
+	
+	@Override 
+	public int[] neededTokens(Map.Entry<Gem, Integer> cost, Map<Gem, Integer> playerGems, Player player) throws RemoteException {
+		int[] totalTokensNeeded = new int[3];
+		int jokersNeeded = 0;
+		int tokensNeeded = 0;
+		
+		if(playerGems.get(cost.getKey()) < (cost.getValue() - player.getBonus().get(cost.getKey()))) {
+			tokensNeeded = playerGems.get(cost.getKey());
+			jokersNeeded -= playerGems.get(cost.getKey()) - (cost.getValue() - player.getBonus().get(cost.getKey()));
+		} else {
+			tokensNeeded = (cost.getValue() - player.getBonus().get(cost.getKey()));
+		}
+		
+		totalTokensNeeded[0] = tokensNeeded;
+		totalTokensNeeded[1] = jokersNeeded;
+		
+		System.out.println(cost.getKey() + " tokens: " + tokensNeeded);
+		System.out.println(cost.getKey() + " jokers: " + jokersNeeded);
+		
+		return totalTokensNeeded;
 	}
 	
 	@Override
