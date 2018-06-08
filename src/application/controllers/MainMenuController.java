0e8e12c8b06e84a6e2cbf0831091bab8bb527e4d
Roy van Dijk <roy.van.dijk@outlook.com>
2018-06-08 13:01:57 +0200

    Javadoc

--- src/application/controllers/MainMenuController.java
+++ src/application/controllers/MainMenuController.java
@@ -1,34 +1,26 @@
 package application.controllers;
 
-import java.io.ByteArrayOutputStream;
-import java.io.IOException;
-import java.io.ObjectOutputStream;
 import java.rmi.NotBoundException;
 import java.rmi.RemoteException;
 import java.rmi.registry.LocateRegistry;
 import java.rmi.registry.Registry;
-import java.rmi.server.UnicastRemoteObject;
-
 import application.StageManager;
-import application.domain.Game;
 import application.domain.GameImpl;
 import application.domain.Lobby;
 import application.domain.LobbyImpl;
-import application.domain.Player;
-import application.domain.PlayerImpl;
 import application.views.LobbyView;
-import application.views.MainMenuView;
-import javafx.application.Platform;
-import javafx.scene.layout.Pane;
-import javafx.stage.Stage;
 
 /**
+ * Controls UI flow within the main menu
+ * 
  * @author Roy
  *
  */
 public class MainMenuController {
-	
+
 	/**
+	 * Sends user input (hostIP & playerName) to lobbyController to join the lobby,
+	 * then switches the view
 	 * 
 	 * @param hostIP
 	 * @param playerName
@@ -38,20 +30,23 @@ public class MainMenuController {
 	 */
 	public void joinLobby(String hostIP, String playerName) throws RemoteException, NotBoundException {
 		Lobby lobby = connectToLobby(hostIP);
-		
-/*		System.out.println(lobby.getGame().toString());
-		lobby.getGame().nextTurn();*/
-		
+
+		/*
+		 * System.out.println(lobby.getGame().toString()); lobby.getGame().nextTurn();
+		 */
+
 		System.out.println("Connected to lobby!");
-		
+
 		LobbyController lobbyController = new LobbyController(lobby);
 		lobbyController.connectPlayer(playerName);
 		LobbyView lobbyView = new LobbyView(lobby, lobbyController);
-		
+
 		StageManager.getInstance().switchScene(lobbyView.asPane());
 	}
 
 	/**
+	 * Sends user input (playerName) to lobbyController to create the lobby, loads
+	 * the previous save file, then switches the view
 	 * 
 	 * @param playerName
 	 * @throws RemoteException
@@ -60,15 +55,18 @@ public class MainMenuController {
 	public void hostPreviousGame(String playerName) throws RemoteException {
 		GameImpl game = new GameImpl(3); // TODO: load from binary savefile
 		LobbyImpl lobby = createLobby(game);
-        
-        lobby.createPlayer(playerName);
-        
+
+		lobby.createPlayer(playerName);
+
 		LobbyController lobbyController = new LobbyController(lobby);
 		LobbyView lobbyView = new LobbyView(lobby, lobbyController);
 
 		StageManager.getInstance().switchScene(lobbyView.asPane());
 	}
+
 	/**
+	 * Sends user input (playerName) to lobbyController to create the lobby, loads a
+	 * new save file, then switches the view
 	 * 
 	 * @param playerName
 	 * @throws RemoteException
@@ -76,42 +74,45 @@ public class MainMenuController {
 	 */
 	public void hostNewGame(String playerName) throws RemoteException {
 		GameImpl game = new GameImpl(4);
-/*		try {
-			new ObjectOutputStream(new ByteArrayOutputStream()).writeObject(game); // Test serialization
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} */
+		/*
+		 * try { new ObjectOutputStream(new ByteArrayOutputStream()).writeObject(game);
+		 * // Test serialization } catch (IOException e) { // TODO Auto-generated catch
+		 * block e.printStackTrace(); }
+		 */
 		LobbyImpl lobby = createLobby(game);
-        
-        lobby.createPlayer(playerName);
-        
+
+		lobby.createPlayer(playerName);
+
 		LobbyController lobbyController = new LobbyController(lobby);
 		LobbyView lobbyView = new LobbyView(lobby, lobbyController);
 
 		StageManager.getInstance().switchScene(lobbyView.asPane());
 	}
+
 	/**
+	 * Creates a new lobby
 	 * 
 	 * @param game
 	 * @return
 	 * @throws RemoteException
 	 * LobbyImpl
 	 */
-	private LobbyImpl createLobby(GameImpl game) throws RemoteException
-	{
+	private LobbyImpl createLobby(GameImpl game) throws RemoteException {
 		LobbyImpl lobby = new LobbyImpl(game);
-		//Lobby lobbySkeleton = (Lobby) UnicastRemoteObject.exportObject(lobby, 0); // cast to remote object
+		// Lobby lobbySkeleton = (Lobby) UnicastRemoteObject.exportObject(lobby, 0); //
+		// cast to remote object
 		System.out.println("Lobby skeleton created");
 		Registry registry = LocateRegistry.createRegistry(1099); // default port 1099 // run RMI registry on local host
 		System.out.println("RMI Registry starter");
 		registry.rebind("Lobby", lobby); // bind calculator to RMI registry
-        System.out.println("Lobby skeleton bound");
-        System.out.println("Server running...");
-        
-        return lobby;
+		System.out.println("Lobby skeleton bound");
+		System.out.println("Server running...");
+
+		return lobby;
 	}
+
 	/**
+	 * Connects to a lobby
 	 * 
 	 * @param hostIP
 	 * @return
@@ -119,14 +120,13 @@ public class MainMenuController {
 	 * @throws NotBoundException
 	 * Lobby
 	 */
-	public Lobby connectToLobby(String hostIP) throws RemoteException, NotBoundException
-	{
+	public Lobby connectToLobby(String hostIP) throws RemoteException, NotBoundException {
 		System.out.println("Getting access to RMI registry");
-		Registry registry = LocateRegistry.getRegistry(hostIP); // Default port: 1099				
+		Registry registry = LocateRegistry.getRegistry(hostIP); // Default port: 1099
 		System.out.println("Getting the Lobby stub from registry");
 		Lobby lobby = (Lobby) registry.lookup("Lobby");
 		System.out.println("Connected to RMI server.");
-		
+
 		return lobby;
 	}
 
