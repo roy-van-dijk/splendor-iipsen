0bd4d390853d9bfdd8030f110be114119fa6dada
Alexander van Dam <alexiaan.vd@hotmail.com>
2018-06-11 11:49:12 +0200

    fix conflict with import

--- src/application/domain/PlayerImpl.java
+++ src/application/domain/PlayerImpl.java
@@ -9,6 +9,9 @@ import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
 
+import application.util.Logger;
+import application.util.Logger.Verbosity;
+
 /**
  * The Class PlayerImpl.
  *
