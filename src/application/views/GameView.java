9a7efd5a359af9b29d870a83188cb193b9ba2ec8
Nickmanbear <misterkees@hotmail.com>
2018-06-11 17:02:19 +0200

    voor sanchez

--- src/application/views/GameView.java
+++ src/application/views/GameView.java
@@ -108,14 +108,14 @@ public class GameView extends UnicastRemoteObject implements UIComponent, Disabl
 				{
 					btnReserveCard.setDisable(gameController.reserveCardInventoryFull());
 					btnEndTurn.setDisable(game.getPlayingField().getTempHand().isEmpty());
+					if(game.getEndTurn().returningTokens())
+					{
+						gameController.showReturnTokensWindow();
+					}	
 				}
 				if(game.getWinningPlayer() != null) {
 					new PopUpWindowView(game.getWinningPlayer().getName() + " has won the game!", "A player has won");
-				}
-				if(game.getEndTurn().returningTokens())
-				{
-					gameController.showReturnTokensWindow();
-				}			
+				}		
 			} catch (RemoteException e) {
 				// TODO Auto-generated catch block
 				e.printStackTrace();
