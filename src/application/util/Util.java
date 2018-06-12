87df4df2728aca6abf031f1f455fdd21448ce3f6
Sanchez <sanchezhuisraad@gmail.com>
2018-06-12 15:49:18 +0200

    Begin with unit testing cardrow

--- src/application/util/Util.java
+++ src/application/util/Util.java
@@ -7,12 +7,8 @@ import java.util.Map;
 import java.util.Map.Entry;
 import java.util.Objects;
 
-// TODO: Auto-generated Javadoc
+
 /** 
- * This is a generic helper class
- * Traditionally, this is against the OOP paradigm as it is procedural programming
- * I reckon we evaluate the necessity of this class at a later stage and make a decision whether to keep it or not.
- * 
  * @author Sanchez 
  */
 
